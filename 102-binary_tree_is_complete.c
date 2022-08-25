45af6ae1de4bf4dd9b2d2900e3f672ef8032626a
Kehinde Bobade <kennygbeminiyi@gmail.com>
2022-08-25 23:06:46 +0100

    fixes;

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -21,7 +21,8 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 	}
 	if (tree->left)
 	{
-		return (binary_tree_is_complete(tree->left) && binary_tree_is_complete(tree->right));
+		return (binary_tree_is_complete(tree->left)
+		&& binary_tree_is_complete(tree->right));
 	}
 	return (0);
 }
--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -38,7 +38,8 @@ int is_perfect(const binary_tree_t *tree, int depth, int level)
 	{
 		return (0);
 	}
-	return (is_perfect(tree->left, depth, level + 1) && is_perfect(tree->right, depth, level + 1));
+	return (is_perfect(tree->left, depth, level + 1) &&
+	is_perfect(tree->right, depth, level + 1));
 }
 
 /**
